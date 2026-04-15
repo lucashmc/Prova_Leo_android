2a92b52b04618fa7be4336b9bf424c70d9339397
lucashmc <lucas.henrique_moraes@hotmail.com>
2026-04-15 20:39:30 -0300

    arrumando a versao para executar o aplicativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.room.runtime)
+    // For Java projects in KTS
+    "annotationProcessor"(libs.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
